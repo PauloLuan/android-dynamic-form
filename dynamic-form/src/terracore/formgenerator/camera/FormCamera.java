8eab9c2f1ffff84f2f47a7a498de84d9cf4797bc
Paulo Luan <pauloluan.inova@gmail.com>
2014-09-17 16:16:48 -0300

    Added images preview to FormCamera class.

--- dynamic-form/src/terracore/formgenerator/camera/FormCamera.java
+++ dynamic-form/src/terracore/formgenerator/camera/FormCamera.java
@@ -1,5 +1,6 @@
 package terracore.formgenerator.camera;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,21 +9,28 @@ import makemachine.android.examples.forms.R;
 import terracore.formgenerator.FormWidget;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class FormCamera extends FormWidget implements Serializable {
-        private Button            cameraButton;
-        private LinearLayout      photosPreview;
-        protected int             _priority;
+        private Button               cameraButton;
+        protected int                _priority;
         
-        private static FormCamera instance;
-        private Context           context;
+        private static FormCamera    instance;
+        private Context              context;
         
-        List<String>              photos = new ArrayList<String>();
+        List<String>                 photos = new ArrayList<String>();
+        
+        private HorizontalScrollView horizontalScrollView;
+        private LinearLayout         photosPreview;
         
         public FormCamera(Context context, String propertyName) {
                 super(context, "");
@@ -39,6 +47,18 @@ public class FormCamera extends FormWidget implements Serializable {
                 setOnCameraButtonClickListener();
                 
                 _layout.addView(cameraButton);
+                
+                initializePhotosPreviewLayout();
+        }
+        
+        public void initializePhotosPreviewLayout() {
+                horizontalScrollView = new HorizontalScrollView(context);
+                photosPreview = new LinearLayout(context);
+                
+                photosPreview.setOrientation(LinearLayout.HORIZONTAL);
+                
+                horizontalScrollView.addView(photosPreview);
+                _layout.addView(horizontalScrollView);
         }
         
         public void setOnCameraButtonClickListener() {
@@ -71,11 +91,96 @@ public class FormCamera extends FormWidget implements Serializable {
         }
         
         public void updatePhotos() {
-                // TODO: update the photos layout
+                photosPreview.removeAllViews();
+                
+                for (String photoPath : photos) {
+                        if (new File(photoPath).exists()) {
+                                ImageView imageView = new ImageView(context);
+                                imageView.setPadding(10, 10, 10, 10);
+                                
+                                Bitmap bitmap = decodeSampledBitmapFromResource(photoPath, 200, 200);
+                                imageView.setImageBitmap(bitmap);
+                                
+                                addEventToImageView(imageView, photoPath);
+                                
+                                photosPreview.addView(imageView);
+                        }
+                }
+        }
+        
+        public ImageView createImageView(String photoPath) {
+                ImageView imageView = new ImageView(context);
+                imageView.setPadding(10, 10, 10, 10);
+                
+                Bitmap bitmap = decodeSampledBitmapFromResource(photoPath, 200, 200);
+                imageView.setImageBitmap(bitmap);
+                
+                addEventToImageView(imageView, photoPath);
+                
+                return imageView;
+        }
+        
+        public void addEventToImageView(
+                                        ImageView imageView,
+                                        final String photoPath) {
+                
+                imageView.setOnClickListener(new OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                Uri uri = Uri.parse(photoPath);
+                                
+                                Intent intent = new Intent();
+                                intent.setDataAndType(uri, "image/*");
+                                intent.setAction(Intent.ACTION_VIEW);
+                                context.startActivity(intent);
+                        }
+                });
         }
         
         public static FormCamera getInstance() {
                 return instance;
+        }
+        
+        public static Bitmap decodeSampledBitmapFromResource(
+                                                             String path,
+                                                             int reqWidth,
+                                                             int reqHeight) {
+                
+                // First decode with inJustDecodeBounds=true to check dimensions
+                final BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(path, options);
+                
+                // Calculate inSampleSize
+                options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+                
+                // Decode bitmap with inSampleSize set
+                options.inJustDecodeBounds = false;
+                return BitmapFactory.decodeFile(path, options);
+        }
+        
+        public static int calculateInSampleSize(
+                                                BitmapFactory.Options options,
+                                                int reqWidth,
+                                                int reqHeight) {
+                // Raw height and width of image
+                final int height = options.outHeight;
+                final int width = options.outWidth;
+                int inSampleSize = 1;
+                
+                if (height > reqHeight || width > reqWidth) {
+                        
+                        final int halfHeight = height / 2;
+                        final int halfWidth = width / 2;
+                        
+                        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+                        // height and width larger than the requested height and width.
+                        while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+                                inSampleSize *= 2;
+                        }
+                }
+                
+                return inSampleSize;
         }
         
 }
